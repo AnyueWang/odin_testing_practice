940d6bde3404b49e43da2a4772adc3ac25f58fa1
AnyueWang <moonwong.way@gmail.com>
2024-07-09 09:59:32 +0200

    add caesar cipher

--- stringProcessor.js
+++ stringProcessor.js
@@ -10,7 +10,26 @@ function reverse(str) {
     return reversedStr
 }
 
+function caesarCipher(str, shift) {
+    let ciphedStr = ''
+    const upperBegin = 65, lowerBegin = 97, numAlphabel = 26
+    for (let i = 0; i < str.length; i++) {
+        let shiftedCharCode
+        const charCode = str.charCodeAt(i)
+        if (upperBegin <= charCode && charCode < upperBegin + numAlphabel) {
+            shiftedCharCode = (charCode + shift - upperBegin) % numAlphabel + upperBegin
+        } else if (lowerBegin <= charCode && charCode < lowerBegin + numAlphabel) {
+            shiftedCharCode = (charCode + shift - lowerBegin) % numAlphabel + lowerBegin
+        } else {
+            shiftedCharCode = charCode
+        }
+        ciphedStr += String.fromCharCode(shiftedCharCode)
+    }
+    return ciphedStr
+}
+
 export {
     capitalize,
     reverse,
+    caesarCipher,
 }
--- testing.test.js
+++ testing.test.js
@@ -1,28 +1,52 @@
-import {capitalize, reverse} from './stringProcessor'
+import { capitalize, reverse, caesarCipher } from './stringProcessor'
 import calculator from './calculator'
 
-test('input "hello", we got "Hello"', ()=>{
-    const capitalizedStr = capitalize('hello')
-    expect(capitalizedStr).toBe('Hello')
+describe('basic string processor', () => {
+    test('input "hello", we got "Hello"', () => {
+        const capitalizedStr = capitalize('hello')
+        expect(capitalizedStr).toBe('Hello')
+    })
+
+    test('input "hello", we got "olleh"', () => {
+        const reversedStr = reverse('hello')
+        expect(reversedStr).toBe('olleh')
+    })
 })
 
-test('input "hello", we got "olleh"', ()=>{
-    const reversedStr = reverse('hello')
-    expect(reversedStr).toBe('olleh')
+describe('calculators', () => {
+    test('2 adding 4 is 6', () => {
+        expect(calculator.add(2, 4)).toBe(6)
+    })
+
+    test('2 subtracting 4 is -2', () => {
+        expect(calculator.subtract(2, 4)).toBe(-2)
+    })
+
+    test('2 multiplying 4 is 8', () => {
+        expect(calculator.multiply(2, 4)).toBe(8)
+    })
+
+    test('2 dividing 4 is 0.5', () => {
+        expect(calculator.divide(2, 4)).toBe(0.5)
+    })
 })
 
-test('2 adding 4 is 6', ()=>{
-    expect(calculator.add(2,4)).toBe(6)
+describe('caesar cipher', () => {
+    test('caesar cipher processing "xyz" with shift factor 3 is "abc"', () => {
+        const ciphedText = caesarCipher('xyz', 3)
+        expect(ciphedText).toBe('abc')
+    })
+
+    test('caesar cipher processing "HeLLo" with shift factor 3 is "KhOOr"', () => {
+        const ciphedText = caesarCipher('HeLLo', 3)
+        expect(ciphedText).toBe('KhOOr')
+    })
+
+    test('caesar cipher processing "Hello, World!" with shift factor 3 is "Khoor, Zruog!"', () => {
+        const ciphedText = caesarCipher('Hello, World!', 3)
+        expect(ciphedText).toBe('Khoor, Zruog!')
+    })
 })
 
-test('2 subtracting 4 is -2', ()=>{
-    expect(calculator.subtract(2,4)).toBe(-2)
-})
 
-test('2 multiplying 4 is 8', ()=>{
-    expect(calculator.multiply(2,4)).toBe(8)
-})
 
-test('2 dividing 4 is 0.5', ()=>{
-    expect(calculator.divide(2,4)).toBe(0.5)
-})
