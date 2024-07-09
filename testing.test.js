fc8079b85c18d3f022bf8beabcad530b7d906356
AnyueWang <moonwong.way@gmail.com>
2024-07-09 09:07:39 +0200

    add calculator object

--- testing.test.js
+++ testing.test.js
@@ -1,4 +1,5 @@
 import {capitalize, reverse} from './stringProcessor'
+import calculator from './calculator'
 
 test('input "hello", we got "Hello"', ()=>{
     const capitalizedStr = capitalize('hello')
@@ -8,4 +9,20 @@ test('input "hello", we got "Hello"', ()=>{
 test('input "hello", we got "olleh"', ()=>{
     const reversedStr = reverse('hello')
     expect(reversedStr).toBe('olleh')
+})
+
+test('2 adding 4 is 6', ()=>{
+    expect(calculator.add(2,4)).toBe(6)
+})
+
+test('2 subtracting 4 is -2', ()=>{
+    expect(calculator.subtract(2,4)).toBe(-2)
+})
+
+test('2 multiplying 4 is 8', ()=>{
+    expect(calculator.multiply(2,4)).toBe(8)
+})
+
+test('2 dividing 4 is 0.5', ()=>{
+    expect(calculator.divide(2,4)).toBe(0.5)
 })
