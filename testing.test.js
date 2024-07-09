ea24861f7590d9c2ae40be6dc1b6e66f368a0fb2
AnyueWang <moonwong.way@gmail.com>
2024-07-09 10:06:43 +0200

    add caesar cipher

--- testing.test.js
+++ testing.test.js
@@ -48,5 +48,15 @@ describe('caesar cipher', () => {
     })
 })
 
-
+describe('array analyzer', () => {
+    test('provide analytical data for an array of numbers', () => {
+        const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+        expect(object).toEqual({
+            average: 4,
+            min: 1,
+            max: 8,
+            length: 6
+        })
+    })
+})
 
